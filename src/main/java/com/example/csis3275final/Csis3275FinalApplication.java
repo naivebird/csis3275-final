ad2fa905e187147bf713cae4c4d143df70dda385
Duc Ho <hoduc1202@gmail.com>
2024-08-12 16:11:21 -0700

    First commit

---
+++ src/main/java/com/example/csis3275final/Csis3275FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.csis3275final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csis3275FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csis3275FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csis3275final/Csis3275FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csis3275final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csis3275FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
